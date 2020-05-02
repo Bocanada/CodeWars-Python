ba50fbbaa52e26beaac09cb29232da5f0c8d9756
Wiped Out <mauromathers@icloud.com>
2020-05-02 17:36:41 -0300

    Changed code

--- Solutions/Tribonacci Sequence.py
+++ Solutions/Tribonacci Sequence.py
@@ -1,31 +1,5 @@
-
-
-def tribonacci(signature, n):
-    # Clever way...
-    Sum = signature[:n]
-    for i in range(n - 3): Sum.append(sum(Sum[-3:]))
-    return Sum
-
-    # seq = []
-#     # if n == 0:
-#     #     return []
-#     # elif n == 1:
-#     #     seq.append(signature[n-1])
-#     #     return seq
-#     # else:
-#     #     count = 3
-#     #     a = signature[0]
-#     #     b = signature[1]
-#     #     c = signature[2]
-#     #     while count < n:
-#     #         z = a + b + c
-#     #         a = b
-#     #         b = c
-#     #         c = z
-#     #         count += 1
-#     #         seq.append(z)
-#     #     return signature + seq
-
-
-
-print(tribonacci([1, 1, 1], 10))
+def Tribonacci(signature, n):
+    summa = signature[:n]
+    for i in range(n - 3):
+        summa.append(sum(summa[-3:]))
+    return summa
