9fb3d18529df1bedb21eb0f5995954081c143b95
Wiped Out <mauromathers@icloud.com>
2020-05-02 18:19:56 -0300

    Added Calculating with functions.py

--- Solutions/Calculating with functions.py
+++ Solutions/Calculating with functions.py
@@ -1,15 +1,68 @@
-def zero(): #your code here
-def one(): #your code here
-def two(): #your code here
-def three(): #your code here
-def four(): #your code here
-def five(): #your code here
-def six(): #your code here
-def seven(): #your code here
-def eight(): #your code here
-def nine(): #your code here
+def zero(f=None):
+    # your code here
+    return 0 if not f else f(0)
 
-def plus(): #your code here
-def minus(): #your code here
-def times(): #your code here
-def divided_by(): #your code here
+
+def one(f=None):
+    # your code here
+    return 1 if not f else f(1)
+
+
+def two(f=None):
+    # your code here
+    return 2 if not f else f(2)
+
+
+def three(f=None):
+    # your code here
+    return 3 if not f else f(3)
+
+
+def four(f=None):
+    # your code here
+    return 4 if not f else f(4)
+
+
+def five(f=None):
+    # your code here
+    return 5 if not f else f(5)
+
+
+def six(f=None):
+    # your code here
+    return 6 if not f else f(6)
+
+
+def seven(f=None):
+    # your code here
+    return 7 if not f else f(7)
+
+
+def eight(f=None):
+    # your code here
+    return 8 if not f else f(8)
+
+
+def nine(f=None):
+    # your code here
+    return 9 if not f else f(9)
+
+
+def plus(y):
+    # your code here
+    return lambda x: x + y
+
+
+def minus(y):
+    # your code here
+    return lambda x: x - y
+
+
+def times(y):
+    # your code here
+    return lambda x: x * y
+
+
+def divided_by(y):
+    # your code here
+    return lambda x: x // y
