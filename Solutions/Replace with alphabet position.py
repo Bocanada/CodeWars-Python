4a04e836f86de2c4f513c4c6f8ebd83eebf748da
Wiped Out <mauromathers@icloud.com>
2020-05-05 15:49:48 -0300

    Error handling ???

--- Solutions/Replace with alphabet position.py
+++ Solutions/Replace with alphabet position.py
@@ -1,2 +1,5 @@
 def alphabet_position(text: str):
-    return ' '.join(list(str(ord(c) - 96) for c in text.lower()))
+    try:
+        return ' '.join(list(str(ord(c) - 96) for c in text.lower().replace(' ', '')))
+    except ValueError and AttributeError:
+        return 'Only alphabet letters are allowed.'
