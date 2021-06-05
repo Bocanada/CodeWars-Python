3186ee4ae906483474d90bd8b681e699b6bd41a5
Bocanada <24578415+Bocanada@users.noreply.github.com>
2021-06-04 23:58:21 -0300

    start from 3 instead of 0

--- Solutions/Multiples of 3 or 5.py
+++ Solutions/Multiples of 3 or 5.py
@@ -1,2 +1,2 @@
 def solution(number: int) -> int:
-    return sum({x for x in range(1, number) if x % 3 == 0 or x % 5 == 0})
+    return sum({x for x in range(3, number) if x % 3 == 0 or x % 5 == 0})
